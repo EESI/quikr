e36cc91bad7bfdb73c5cbca73f4f2e07d870f7cc
Calvin <calvin@EESI>
2013-03-05 12:55:10 -0500

    simplify generate_kmers.py

--- generate_kmers.py
+++ generate_kmers.py
@@ -2,6 +2,4 @@
 import itertools
 import sys
 
-kmer_array ='\n'.join(''.join(x) for x in itertools.product('acgt', repeat=int(sys.argv[1])))
-
-print kmer_array
+print'\n'.join(''.join(x) for x in itertools.product('acgt', repeat=int(sys.argv[1])))
