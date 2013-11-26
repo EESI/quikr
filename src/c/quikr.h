50f42a6697a6dedbfd4f0737568e816077010253
Calvin Morrison <mutantturkey@gmail.com>
2013-11-26 10:37:43 -0500

    some spacing changes

--- src/c/quikr.h
+++ src/c/quikr.h
@@ -1,9 +1,10 @@
 #define MATRIX_REVISION 0
 #define pow_four(x) ( (unsigned long long)1 << (x * 2 ) )
-
+#define str_eq(s1,s2)  (!strcmp ((s1),(s2)))
 struct matrix {
 	unsigned long long sequences;
 	unsigned int kmer;
 	double *matrix;
 	char **headers;
 };
+
--- src/c/quikr_functions.h
+++ src/c/quikr_functions.h
@@ -1,5 +1,6 @@
 // our malloc checker
 void check_malloc(void *ptr, char *error);
+
 // count the sequences in a fasta file
 unsigned long long count_sequences(const char *filename);
 
@@ -8,6 +9,3 @@ void normalize_matrix(double *matrix, int height, int width);
 
 // load a sensing matrix  
 struct matrix *load_sensing_matrix(const char *filename, unsigned int target_kmer);
-
-// add header and normalize count matrix
-double *setup_count_matrix(char *filename, unsigned long long kmer, unsigned long long lambda, unsigned long long width);
